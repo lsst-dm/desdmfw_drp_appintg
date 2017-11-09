135161a6062647c4d522139d332c5a7fac6b9fd8
Michelle Gower <mgower@illinois.edu>
2017-11-09 11:34:59 -0600

    Initial commit of intg codes to run DRP in DESDM
    Initial commit of integration codes needed to run DRP pipeline
    in DESDM framework.   Currently includes a specialized
    query code used for finding inputs in the "data backbone"
    for running processCcd.py.

---
+++ setup.py
@@ -0,0 +1,17 @@
+import distutils
+from distutils.core import setup
+import glob
+
+bin_files = glob.glob("bin/*")
+
+# The main call
+setup(name='desdmfw_drp_appintg',
+      version ='0.0.1',
+      license = "GPL",
+      description = "Application integration codes for running the DRP pipeline in the DESDM framework",
+      author = "Michelle Gower",
+      author_email = "mgower@illinois.edu",
+      scripts = bin_files,
+      data_files=[('ups',['ups/desdmfw_drp_appintg.table'])]
+      )
+
