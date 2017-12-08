909c08cd46a5d4601a54d93faf4c2edf90d48a0a
Mikolaj Kowalik <mxk@illinois.edu>
2017-12-08 10:47:51 -0600

    Clean up the code using autopep8

--- bin/processccd_query.py
+++ bin/processccd_query.py
@@ -11,46 +11,48 @@ from intgutils import queryutils
 from despydb import desdbi
 
 ######################################################################
+
+
 def query_db(args):
 
     rawinfo = {}
     dbh = desdbi.DesDbi(None, args.section)
-    #sql = "select filename,taiObs,expId,pointing,visit,dateObs,frameId,filter,field,pa,exptime,ccdtemp,ccd,proposal,config,autoguider from image where filetype='hsc_raw' and visit=%s and ccd=%s" % (dbh.get_named_bind_string('visit'), dbh.get_named_bind_string('ccd')) 
+    #sql = "select filename,taiObs,expId,pointing,visit,dateObs,frameId,filter,field,pa,exptime,ccdtemp,ccd,proposal,config,autoguider from image where filetype='hsc_raw' and visit=%s and ccd=%s" % (dbh.get_named_bind_string('visit'), dbh.get_named_bind_string('ccd'))
 
-    rawfields = miscutils.fwsplit(args.raw_select,',')
-    calibfields = miscutils.fwsplit(args.calib_select,',')
+    rawfields = miscutils.fwsplit(args.raw_select, ',')
+    calibfields = miscutils.fwsplit(args.calib_select, ',')
 
     imgselect = []
     for f in rawfields:
-        imgselect.append('i.%s as img_%s' % (f,f))
-        
+        imgselect.append('i.%s as img_%s' % (f, f))
+
     calibselect = []
     for f in calibfields:
-        calibselect.append('c.%s as calib_%s' % (f,f))
+        calibselect.append('c.%s as calib_%s' % (f, f))
 
     fromtables = ['ops_calibration_lookup l', 'calibration c', 'ccd_overlap_patch o', 'image i']
     if args.visittag is not None:
         fromtables.append('visit_tag vt')
 
     # Note do not need blacklist in main from clause
-        
+
     sql = """select distinct o.tract, %s, %s 
              from %s
              where c.filename=l.filename and i.filetype=%s and i.visit=o.visit and 
              i.ccd=o.ccd and l.min_date <= to_date(i.dateobs, 'YYYY-MM-DD') and 
              to_date(i.dateobs, 'YYYY-MM-DD') <= l.max_date and i.ccd=c.ccd and 
              (c.filter is null or c.filter=i.filter) and o.version=%s and l.version=%s and o.tract = %s""" % \
-          (','.join(imgselect), 
-           ','.join(calibselect), 
+          (','.join(imgselect),
+           ','.join(calibselect),
            ','.join(fromtables),
-           dbh.get_named_bind_string('raw_filetype'), 
-           dbh.get_named_bind_string('overlap_version'), 
+           dbh.get_named_bind_string('raw_filetype'),
+           dbh.get_named_bind_string('overlap_version'),
            dbh.get_named_bind_string('calib_version'),
            dbh.get_named_bind_string('tract'))
-    bindvals = {'tract': args.tract, 'raw_filetype': args.raw_filetype, 
+    bindvals = {'tract': args.tract, 'raw_filetype': args.raw_filetype,
                 'overlap_version': args.overlap_version,
                 'calib_version': args.calib_version}
-    
+
     if args.filter is not None:
         sql += " and i.filter in ('%s') " % ("','".join(miscutils.fwsplit(args.filter, ',')))
 
@@ -68,19 +70,20 @@ def query_db(args):
         bindvals['ccd'] = args.ccd
 
     if args.visittag is not None:
-        sql += " and vt.tag in ('%s') and vt.visit=i.visit and vt.ccd=i.ccd" % ("','".join(miscutils.fwsplit(args.visittag, ',')))
+        sql += " and vt.tag in ('%s') and vt.visit=i.visit and vt.ccd=i.ccd" % (
+            "','".join(miscutils.fwsplit(args.visittag, ',')))
 
     if not args.blacklist:
         sql += " and not exists (select visit from blacklist bl where i.visit=bl.visit and i.ccd=bl.ccd) "
 
     if args.blacklist_code is not None:
-        sql += " and not exists (select visit from blacklist bl where i.visit=bl.visit and i.ccd=bl.ccd and bl.reason_code in (%s)) " % (",".join(miscutils.fwsplit(args.blacklist_code, ',')))
+        sql += " and not exists (select visit from blacklist bl where i.visit=bl.visit and i.ccd=bl.ccd and bl.reason_code in (%s)) " % (
+            ",".join(miscutils.fwsplit(args.blacklist_code, ',')))
 
     print "sql =", sql
 
     curs = dbh.cursor()
     curs.prepare(sql)
-
 
     flabelset = set({'raw'})
     curs.execute(None, bindvals)
@@ -92,26 +95,27 @@ def query_db(args):
         # since writing to WCL, convert NULL to __KEEP__NONE__
         for k in cinfo:
             if cinfo[k] is None:
-                cinfo[k] = '__KEEP__NONE__' 
+                cinfo[k] = '__KEEP__NONE__'
 
-        
         if cinfo['img_filename'] not in rawinfo:
             rawinfo[cinfo['img_filename']] = {}
             rawdict = {'imgname': cinfo['img_filename']}
             for f in rawfields:
                 rawdict[f] = cinfo['img_%s' % f]
-             
+
             rawinfo[cinfo['img_filename']]['raw'] = rawdict
-             
+
         calibdict = {'imgname': cinfo['img_filename']}
-        for f in calibfields: 
+        for f in calibfields:
             calibdict[f] = cinfo['calib_%s' % f]
         rawinfo[cinfo['img_filename']][calibdict['filetype']] = calibdict
         flabelset.add(calibdict['filetype'])
-                
+
     return rawinfo, list(flabelset)
 
 ######################################################################
+
+
 def reformat_data(rawinfo, filelabels):
     """ change data structure of queried data to meet requirements of convert_multiple_files_to_lines """
 
@@ -123,11 +127,12 @@ def reformat_data(rawinfo, filelabels):
         newdata.append(lineinfo)
 
     return newdata
-    
+
 
 ######################################################################
 def main(argv):
-    parser = argparse.ArgumentParser(description='Dummy query that reads fake query results from a semi-colon separated file')
+    parser = argparse.ArgumentParser(
+        description='Dummy query that reads fake query results from a semi-colon separated file')
     parser.add_argument('--qoutfile', required=True, action='store')
     parser.add_argument('--qouttype', action='store', default='wcl')
     parser.add_argument('--section', action='store')
@@ -135,16 +140,24 @@ def main(argv):
     parser.add_argument('--overlap_version', required=True, action='store')
     parser.add_argument('--calib_version', required=True, action='store')
     parser.add_argument('--raw_filetype', default='hsc_raw', action='store')
-    parser.add_argument('--calib_select', default='filename, filter, ccd, filetype, calib_date', action='store')
-    parser.add_argument('--raw_select', default='filename, filter, visit, ccd, pointing, dateobs, field', action='store')
+    parser.add_argument(
+        '--calib_select', default='filename, filter, ccd, filetype, calib_date', action='store')
+    parser.add_argument(
+        '--raw_select', default='filename, filter, visit, ccd, pointing, dateobs, field', action='store')
     parser.add_argument('--tract', required=True, action='store', type=int)
-    parser.add_argument('--patch', required=False, action='store', help='Must match the patch string format in overlap table')
-    parser.add_argument('--filter', required=False, action='store', help="Include only certain filters, comma-separated list")
-    parser.add_argument('--visit', required=False, action='store', help="Include only certain visits, comma-separated list")
-    parser.add_argument('--ccd', required=False, action='store', help="Include only certain ccds - note doesn't support visit+ccd pairs")
+    parser.add_argument('--patch', required=False, action='store',
+                        help='Must match the patch string format in overlap table')
+    parser.add_argument('--filter', required=False, action='store',
+                        help="Include only certain filters, comma-separated list")
+    parser.add_argument('--visit', required=False, action='store',
+                        help="Include only certain visits, comma-separated list")
+    parser.add_argument('--ccd', required=False, action='store',
+                        help="Include only certain ccds - note doesn't support visit+ccd pairs")
     parser.add_argument('--visittag', required=False, action='store')
-    parser.add_argument('--blacklist', required=False, action='store_false', help="Doesn't include if exists in blacklist table")
-    parser.add_argument('--blacklist_code', required=False, action='store', help="Use if only want to blacklist for certain codes, comma-separated list")
+    parser.add_argument('--blacklist', required=False, action='store_false',
+                        help="Doesn't include if exists in blacklist table")
+    parser.add_argument('--blacklist_code', required=False, action='store',
+                        help="Use if only want to blacklist for certain codes, comma-separated list")
     args = parser.parse_args(argv)
 
     data, filelabels = query_db(args)
--- setup.py
+++ setup.py
@@ -6,12 +6,11 @@ bin_files = glob.glob("bin/*")
 
 # The main call
 setup(name='desdmfw_drp_appintg',
-      version ='0.0.1',
-      license = "GPL",
-      description = "Application integration codes for running the DRP pipeline in the DESDM framework",
-      author = "Michelle Gower",
-      author_email = "mgower@illinois.edu",
-      scripts = bin_files,
-      data_files=[('ups',['ups/desdmfw_drp_appintg.table'])]
+      version='0.0.1',
+      license="GPL",
+      description="Application integration codes for running the DRP pipeline in the DESDM framework",
+      author="Michelle Gower",
+      author_email="mgower@illinois.edu",
+      scripts=bin_files,
+      data_files=[('ups', ['ups/desdmfw_drp_appintg.table'])]
       )
-
